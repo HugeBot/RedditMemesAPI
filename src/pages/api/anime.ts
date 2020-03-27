31d84ada3bc29a5b1c657ab0207f42109e7705e0
Blad3Mak3r <blademaker.live@gmail.com>
2020-03-27 12:36:28 +0100

    New anime endpoint

---
+++ src/pages/api/anime.ts
@@ -0,0 +1,7 @@
+import { NowRequest, NowResponse } from '@now/node'
+import Axios from 'axios';
+import { randomNumber, checkURL } from '../../utils';
+import { Meme } from '../../interfaces/Meme';
+
+export default async (req: NowRequest, res: NowResponse) => {
+}
