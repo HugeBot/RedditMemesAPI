878c48d6e994463db23a912fb292cb53fa2f24e1
Blad3Mak3r <blademaker.live@gmail.com>
2020-04-14 23:20:57 +0200

    Deletions

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function randomNumber(number: number): number {
-    return Math.floor(Math.random() * number);
-}
-
-export function checkURL(url: string): boolean {
-    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
-}
